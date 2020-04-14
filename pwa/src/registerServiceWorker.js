3be946352356d75822a7aadbc8df6e4996ac9d94
Marcel Schleinkofer <marcel.schleinkofer@innotix.com>
2020-04-14 12:00:23 +0200

    config

--- pwa/src/registerServiceWorker.js
+++ pwa/src/registerServiceWorker.js
@@ -3,12 +3,12 @@
 import { register } from 'register-service-worker';
 
 if (process.env.NODE_ENV === 'production') {
-  navigator.serviceWorkerContainer.register('service-worker.js').then(function(reg){
-  register(`${process.env.BASE_URL}service-worker.js`, {
+  // register(`${process.env.BASE_URL}service-worker.js`, {
+  register('./service-worker.js', {
     ready() {
       console.log(
         'App is being served from cache by a service worker.\n'
-        + 'For more details, visit https://goo.gl/AFskqB',
+          + 'For more details, visit https://goo.gl/AFskqB',
       );
     },
     registered() {
