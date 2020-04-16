61a0f73aaa71e8795fa5a3233790a9b5aa91fb1f
Marcel Schleinkofer <marcel.schleinkofer@innotix.com>
2020-04-16 17:08:16 +0200

    add dynamic caching

--- ui-vuetify/src/sw.js
+++ ui-vuetify/src/sw.js
@@ -1,3 +1,4 @@
+const DYNAMIC_CACHE = 'dynamicCache';
 // This is the code piece that GenerateSW mode can't provide for us.
 // This code listens for the user's confirmation to update the app.
 self.addEventListener('message', e => {
@@ -14,7 +15,24 @@ self.addEventListener('message', e => {
       break;
   }
 });
-
+self.addEventListener('fetch', e => {
+  var request = e.request;
+  event.respondWith(
+    fetch(request)
+      .then(function(res) {
+        return caches.open(DYNAMIC_CACHE).then(function(cache) {
+          cache.put(event.request.url, res.clone());
+          return res;
+        });
+      })
+      .catch(function(err) {
+        return caches.match(e.request);
+      }),
+    /* caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }), */
+  );
+});
 workbox.core.clientsClaim(); // Vue CLI 4 and Workbox v4, else
 // workbox.clientsClaim(); // Vue CLI 3 and Workbox v3.
 
